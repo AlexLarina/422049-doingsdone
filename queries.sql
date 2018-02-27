3b865e695ee6442d3f2dfb0cbfcc3871c33b8469
Alexandra Larina <aa.larina.ff@gmail.com>
2018-02-27 12:04:35 +0300

    Исправлены sql запросы на более компактные. Исправлен формат добавления даты.

--- queries.sql
+++ queries.sql
@@ -1,40 +1,25 @@
 -- filling projects table
-
-INSERT INTO projects SET name = 'Все', user_id = 1;
-INSERT INTO projects SET name = 'Все', user_id = 2;
-INSERT INTO projects SET name = 'Все', user_id = 3;
-
-INSERT INTO projects SET name = 'Входящие', user_id = 1;
-INSERT INTO projects SET name = 'Входящие', user_id = 2;
-INSERT INTO projects SET name = 'Входящие', user_id = 3;
-
-INSERT INTO projects SET name = 'Учеба', user_id = 1;
-INSERT INTO projects SET name = 'Работа', user_id = 1;
-
-INSERT INTO projects SET name = 'Учеба', user_id = 2;
-INSERT INTO projects SET name = 'Домашние дела', user_id = 2;
-
-INSERT INTO projects SET name = 'Работа', user_id = 3;
-INSERT INTO projects SET name = 'Авто', user_id = 3;
+INSERT INTO projects (name, user_id) VALUES ('Все', 1), ('Все', 2), ('Все', 3), ('Входящие', 1), ('Входящие', 2),
+  ('Входящие', 3), ('Учеба', 1), ('Работа', 1), ('Учеба', 2), ('Домашние дела', 2), ('Работа', 3), ('Авто', 3);
 
 -- filling users table
 
-INSERT INTO users SET email = 'ignat.v@gmail.com', name = 'Игнат', password = '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka';
-INSERT INTO users SET email = 'kitty_93@li.ru', name = 'Леночка', password = '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa';
-INSERT INTO users SET email = 'warrior07@mail.ru', name = 'Руслан', password = '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW';
+INSERT INTO users (email, name, password) VALUES
+  ('ignat.v@gmail.com', 'Игнат', '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka'),
+  ('kitty_93@li.ru', 'Леночка', '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa'),
+  ('warrior07@mail.ru', 'Руслан', '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW');
 
 -- filling tasks table
 
-INSERT INTO tasks SET name = 'Собеседование в IT компании', dt_deadline = '01.06.2018', user_id = 1, project_id = 8;
-INSERT INTO tasks SET name = 'Выполнить тестовое задание', dt_deadline = '25.05.2018', user_id = 3, project_id = 11;
-INSERT INTO tasks SET name = 'Сделать задание первого раздела', dt_deadline = '21.04.2018', user_id = 2, project_id = 9;
-
-INSERT INTO tasks SET name = 'Встреча с другом', dt_deadline = '22.04.2018', user_id = 1, project_id = 4;
-INSERT INTO tasks SET name = 'Встреча с другом', dt_deadline = '22.04.2018', user_id = 2, project_id = 5;
-INSERT INTO tasks SET name = 'Встреча с другом', dt_deadline = '22.04.2018', user_id = 3, project_id = 6;
-
-INSERT INTO tasks SET name = 'Купить корм для кота', dt_deadline = NULL, user_id = 2, project_id = 10;
-INSERT INTO tasks SET name = 'Заказать пиццу', dt_deadline = NULL, user_id = 2, project_id = 10;
+INSERT INTO tasks (name, dt_deadline, user_id, project_id) VALUES
+  ('Собеседование в IT компании', '2018.06.01', 1, 8),
+  ('Выполнить тестовое задание', '2018.05.25', 3, 11),
+  ('Сделать задание первого раздела', '2018.04.21', 2, 9),
+  ('Встреча с другом', '2018.04.22', 1, 4),
+  ('Встреча с другом', '2018.04.22', 2, 5),
+  ('Встреча с другом', '2018.04.22', 3, 6),
+  ('Купить корм для кота', NULL, 2, 10),
+  ('Заказать пиццу', NULL, 2, 10);
 
 -- select
 
